2f33bc6cd8923c5dda4f1b146144a4693ddeba1c
Thijmen <thijmenhensbergen@gmail.com>
2026-03-10 16:31:42 +0100

    Added shaders and some extra code to the movement
    I added a water shader and a pixel art post processing shader to make the game look a little bit cleaner. I also made it so the player looks in the direction of movement (and i gave her sunglasses)

--- Entities/Player/Player.cs
+++ Entities/Player/Player.cs
@@ -6,6 +6,11 @@ public partial class Player : CharacterBody3D
 	public const float Speed = 5.0f;
 	public const float JumpVelocity = 4.5f;
 
+	[Export] public float rotationSpeed = 2;
+	[Export] public MeshInstance3D body;
+
+	Vector3 lastdirection = Vector3.Forward;
+
 	public override void _PhysicsProcess(double delta)
 	{
 		Vector3 velocity = Velocity;
@@ -16,18 +21,13 @@ public partial class Player : CharacterBody3D
 			velocity += GetGravity() * (float)delta;
 		}
 
-		// Handle Jump.
-		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
-		{
-			velocity.Y = JumpVelocity;
-		}
-
 		// Get the input direction and handle the movement/deceleration.
 		// As good practice, you should replace UI actions with custom gameplay actions.
 		Vector2 inputDir = Input.GetVector("Left", "Right", "Forward", "Backward");
 		Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
 		if (direction != Vector3.Zero)
 		{
+			lastdirection = direction;
 			velocity.X = direction.X * Speed;
 			velocity.Z = direction.Z * Speed;
 		}
@@ -36,8 +36,11 @@ public partial class Player : CharacterBody3D
 			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
 			velocity.Z = Mathf.MoveToward(Velocity.Z, 0, Speed);
 		}
-
+		var bodyrot = body.Rotation;
+		bodyrot.Y = Mathf.LerpAngle(bodyrot.Y, Mathf.Atan2(-lastdirection.X, -lastdirection.Z), (float)delta * rotationSpeed);
+		body.Rotation = bodyrot;
 		Velocity = velocity;
 		MoveAndSlide();
 	}
 }
+
